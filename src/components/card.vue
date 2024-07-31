adbc48fffb405d8dcf4745f8ee3f176fa39293ee
Ararabots <ararabots@gmail.com>
2024-07-31 12:55:45 -0400

    Amanda-fix: card menu screen

--- src/components/card.vue
+++ src/components/card.vue
@@ -1,8 +1,21 @@
 <template>
   <div class="card-list">
     <div class="card" v-for="(card, index) in cards" :key="index">
-      <h2>{{ card.title }}</h2>
-      <p>{{ card.description }}</p>
+      <div class="card-icon">
+        <h2>{{ index + 1 }}</h2>
+      </div>
+      <div class="card-line">
+      </div>
+      <div class="card-name">
+        <h2>{{ card.name }}</h2>
+      </div>
+      <div class="card-position">
+        <select id="options" class="dropdown-position" v-model="selectedOption" @change="updateCardList">
+          <option value="1">Atacante</option>
+          <option value="2">Goleiro</option>
+          <option value="3">Zagueiro</option>
+        </select>
+      </div>      
     </div>
   </div>
 </template>
@@ -12,12 +25,12 @@ export default {
   data() {
     return {
       cards: [
-        { title: 'Card 1', description: 'Descrição do Card 1.' },
-        { title: 'Card 2', description: 'Descrição do Card 2.' },
-        { title: 'Card 3', description: 'Descrição do Card 3.' },
-        { title: 'Card 4', description: 'Descrição do Card 4.' },
-        { title: 'Card 5', description: 'Descrição do Card 5.' },
-        { title: 'Card 6', description: 'Descrição do Card 6.' }
+        { name: 'Robô 1', position: 'Descrição do Card 1.' },
+        { name: 'Robô 2', position: 'Descrição do Card 2.' },
+        { name: 'Robô 3', position: 'Descrição do Card 3.' },
+        { name: 'Robô 4', position: 'Descrição do Card 4.' },
+        { name: 'Robô 5', position: 'Descrição do Card 5.' },
+        { name: 'Robô 6', position: 'Descrição do Card 6.' }
       ]
     };
   }
@@ -28,18 +41,52 @@ export default {
 .card {
   background-color: #383f6b;
   border-radius: 8px;
-  padding: 10px;
-  margin-bottom: 15px;
+  margin-bottom: 5%;
   color: #D2D1CB;
+  display: flex;
+  width: 100%;
 }
 
-.card h2 {
-  margin-bottom: 5px;
-  font-size: 19px;
+.card-icon {
+  display: flex;
+  width: 8%;
+  margin: 2%;
+  align-items: center; /* Centraliza o texto verticalmente */
+  justify-content: center; /* Centraliza o texto horizontalmente */
 }
 
-.card p {
-  margin: 0; /* Remover margem padrão do parágrafo */
+.card-line {
+  height: 100%;
+  width: 2%;
+  background-color: green;
+}
+
+.card-name {
+  display: flex;
+  width: 100%;
+  margin-left: 10px;
+  align-items: center; /* Centraliza o texto verticalmente */
+}
+
+.card-position {
+  width: 40%;
+  display: flex;
+  align-items: center; /* Centraliza o texto verticalmente */
+}
+
+.dropdown-position {
+  font-size: 25px;
+  border: 1px;/* solid #252838; /* Cor da borda */
+  border-radius: 4px; /* Cantos arredondados */
+  background-color: #383f6b; /* Cor de fundo do dropdown */
+  color: #D2D1CB; /* Cor do texto */
+  width: 90%; /* Ajusta a largura conforme necessário */
+  height: 75%;
+}
+
+.dropdown-position option {
+  background-color: #252838; /* Cor de fundo das opções */
+  color: #D2D1CB; /* Cor do texto das opções */
 }
 
 .card-list {
